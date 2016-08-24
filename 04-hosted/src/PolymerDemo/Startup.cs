fd170ed193edc82d681b2b8b0aa7f48536a256b4
Christian Bay <taz@nakke.dk>
2016-08-24 21:06:10 +0200

    Demo

---
+++ 04-hosted/src/PolymerDemo/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace PolymerDemo
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseBrowserLink();
+			app.UseCors(x => x.AllowAnyOrigin());
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+			app.UseStatusCodePages();
+		}
+	}
+}
